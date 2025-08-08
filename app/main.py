3dd8540998b02a6b204ba871be1a1e02e5613d46
Ian Warren <ianjohnwarren@gmail.com>
2025-08-08 16:44:31 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,36 +1,16 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import json
-import os
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Static and template setup
+# Mount static folder if it exists (optional)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
-# Load articles from JSON
-def load_articles():
-    data_path = os.path.join("data", "combined_news.json")
-    with open(data_path, encoding="utf-8") as f:
-        return json.load(f)
+# Point to templates folder
+templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def homepage(request: Request, topic: str = None):
-    articles = load_articles()
-
-    # Filter by topic if provided
-    if topic:
-        articles = [a for a in articles if topic in a.get("topics", [])]
-
-    # Gather list of all topics for dropdown
-    all_topics = sorted({t for a in articles for t in a.get("topics", [])})
-
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "articles": articles,
-        "topics": all_topics,
-        "selected_topic": topic
-    })
+async def homepage(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
